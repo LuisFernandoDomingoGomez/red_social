09a681eb0f5b6994e6e5df31b0d4a61c61617583
Luis Fernando Domingo Gómez <al222010066@gmail.com>
2023-02-20 13:24:56 -0600

    Validaciones en Formularios

--- src/app/nuevo-usuario/nuevo-usuario.component.ts
+++ src/app/nuevo-usuario/nuevo-usuario.component.ts
@@ -24,17 +24,44 @@ export class NuevoUsuarioComponent implements OnInit {
 
     this.formNuevoUsuario = this.fb.group({
       nombre: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(10)]],
-      apellidos: [''],
+      apellidos: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(10)]],
       genero:[''],
       correo:['', [Validators.required, Validators.email]],
-      direccion: this.fb.group({
-        calle: [''],
-        ciudad: [''],
-        estado: [''],
+
+        calle: ['', Validators.required],
+        ciudad: ['', Validators.required],
+        estado: ['', Validators.required],
         codigoPostal: ['']
-      }),
     });
 
+  }
+
+  getFormato(componente:string) {
+    let formato:string = 'form-control';
+
+    if (this.formNuevoUsuario.get(componente)?.invalid && this.formNuevoUsuario.get(componente)?.touched) {
+      formato = 'form-control is-invalid';
+    } else if(this.formNuevoUsuario.get(componente)?.valid && this.formNuevoUsuario.get(componente)?.touched) {
+      formato = 'form.control is-valid';
+    }
+    return formato;
+  }
+
+  getErrorMessage(componente:string) {
+    let message: string = '';
+    let length: string = '';
+
+    if (this.formNuevoUsuario.get(componente)?.hasError('minlength')) {
+      message = 'Este campo es requerido';
+    } else if (this.formNuevoUsuario.get(componente)?.hasError('minlength')) {
+
+      length = this.formNuevoUsuario.get(componente)?.errors?.['minlength']?.['requiresLength'];
+      message = 'La longitud minima de este campo es de ${ length } caracteres.';
+    } else if (this.formNuevoUsuario.get(componente)?.hasError('email')) {
+
+      message = 'Correo con formato incorrecto';
+    }
+    return message;
   }
 
 }
